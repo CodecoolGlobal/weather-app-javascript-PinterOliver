405f146007ba0614dc3980e8a1d63f554399245b
Andras Toth <125831134+andrastoth021@users.noreply.github.com>
2023-05-15 14:32:35 +0200

    implement input bar

--- frontend/script.js
+++ frontend/script.js
@@ -1,20 +1,41 @@
 const WEATHER_API_KEY = '664e3a10f526495492f112135231505';
 
 const loadEvent = () => {
-
-
-
+  // MAIN
+  displayInputBar();
 };
 
 window.addEventListener('load', loadEvent);
 
+
+// DOM Manipulations
+
+/**
+   * Creates and displays an input bar HTML element.
+   */
+function displayInputBar() {
+  insertHTML('root', '', 'div', 'navPanel', '', '');
+
+  insertHTML('navPanel', '', 'div', 'inputBox', 'control has-icons-left');
+  // eslint-disable-next-line max-len
+  insertHTML('inputBox', '', 'input', 'search', 'input is-medium', 'placeholder="Type in a city\'s name"');
+  insertHTML('inputBox', '', 'span', '', 'icon is-left', '');
+  /*
+  <div class="control has-icons-left has-icons-right" id='inputBox'>
+    <input class="input is-medium" type="email" placeholder="Email">
+    <span class="icon is-left">
+      <i class="fas fa-envelope"></i>
+    </span>
+  </div>
+   */
+}
 
 
 // Create HTML Elements
 
 /**
    * Creates and displays an HTML element.
-   * @param {object} parentElementId - The ID of the HTML element you want to be the parent of the new HTML element.
+   * @param {string} parentElementId - The ID of the HTML element you want to be the parent of the new HTML element.
    * @param {string} content - The content of the HTML element.
    * @param {string} tag - The tagname of the HTML element.
    * @param {string} id - The id of the HTML element.
