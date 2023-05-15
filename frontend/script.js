3a98cb31a373c2a7c22cd8bd6608ffb4ed401eff
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-05-15 13:13:37 +0200

    setup the repo

---
+++ frontend/script.js
@@ -0,0 +1,7 @@
+const loadEvent = () => {
+  // the HTML elements with ID are available as global variables with the ID (eg. root) but it is better if you
+  const rootElement = document.getElementById('root');
+
+};
+
+window.addEventListener('load', loadEvent);
