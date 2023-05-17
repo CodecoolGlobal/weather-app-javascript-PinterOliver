f41d8303520a7a4e6113afe6b76821d060a9e0f1
Oliver Peter Pinter <125395118+PinterOliver@users.noreply.github.com>
2023-05-17 23:07:30 +0200

    fix inputbox Enter keydown bug

--- frontend/script.js
+++ frontend/script.js
@@ -2,18 +2,77 @@ const WEATHER_API_KEY = '664e3a10f526495492f112135231505';
 const IMAGE_API_KEY = '93pgBPYkOqOAqUyuhTDX7I2NutiObGjRo02Ksv3eqIWuuwcU1CDPSSgo';
 const FAVORITES = [];
 
+/**
+ * The main function.
+*/
 const loadEvent = () => {
-  // MAIN
   displayInputBar();
 };
 
+// Reaction to user input
+
 window.addEventListener('load', loadEvent);
+
+/*function makeTheButtonsClickable() {
+  elementById('dropdown-content').addEventListener('click', (event) => {
+    processClickOnSuggested(event);
+  });
+}
+
+function processClickOnSuggested(event) {
+  console.log(event.target.firstChild.innerText);
+  elementById('search').value = event.target.firstChild.innerText;
+  console.log(elementById('search').value);
+  processWeather(event.target.firstChild.innerText);
+
+  const e = new Event("change");
+const element = document.querySelector('#test')
+element.dispatchEvent(e);
+}*/
+
+function processInputChange() {
+  elementById('search').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      recieveWeather(event);
+      elementById('suggestionDropDown').classList.remove('is-active');
+    }
+  });
+  elementById('search').addEventListener('focusout', (event) => processClickOnSuggested(event));  // Ki kattintás változása
+}
+
+function processAutocomplete() {
+  elementById('search').addEventListener('input', (event) => recieveAutocomplete(event)); // Szöveg változás
+  elementById('search').addEventListener('focusin', (event) => recieveAutocomplete(event)); // Be kattintás változása
+}
+
+function processClickOnSuggested(event) {
+  {
+    setTimeout(function() {
+      if (!document.activeElement.querySelector('button')) {
+        console.log('activeElement.child ', document.activeElement.lastChild);
+        const clickedCity = document.activeElement.lastChild.innerText;
+        event.target.value = clickedCity;
+        processWeather(clickedCity);
+      } else {
+        recieveWeather(event);
+      }
+      elementById('suggestionDropDown').classList.remove('is-active');
+    }, 0);
+  }
+}
+
+function processFavoriteClick() {
+  elementById('favorite').addEventListener('click', changeFavorite);
+}
+
+// Fetch and process information
 
 async function recieveAutocomplete(event) {
   const cityName = event.target.value;
   if (cityName.length >= 3) {
+    const action = 'search';
     const urlParts = {
-      site: 'http://api.weatherapi.com/v1/search.json',
+      site: `http://api.weatherapi.com/v1/${action}.json`,
       key: WEATHER_API_KEY,
       q: `${cityName}*`,
     };
@@ -24,44 +83,48 @@ async function recieveAutocomplete(event) {
   }
 }
 
-async function recieveWeather(event) {
+function recieveWeather(event) {
   // töltődő jel, amíg nem kap adatot
   let cityName = event.target.value;
   const foundFavCities = search(FAVORITES, cityName);
   if (foundFavCities.length > 0) {
     cityName = foundFavCities[0];
   } else if (document.querySelector('.is-active')) {
-    cityName = elementById('city-0').firstChild.innerText;
+    cityName = elementById('city-0').lastChild.innerText;
   }
+  event.target.value = cityName;
+  if (cityName.length >= 3) processWeather(cityName);
+}
 
-  if (cityName.length >= 3) {
-    const NUMBER_OF_DAYS_TO_FORECAST = 8;
-    const NUMBER_OF_PICTURES = 1;
-    const urlPartsWeather = {
-      site: 'http://api.weatherapi.com/v1/forecast.json',
-      key: WEATHER_API_KEY,
-      q: cityName,
-      days: NUMBER_OF_DAYS_TO_FORECAST.toString(),
-      aqi: 'no',
-      alerts: 'no',
-    };
-    const recieved = await getFetchOf(urlPartsWeather);
-    const urlPartsImage = {
-      site: 'https://api.pexels.com/v1/search',
-      query: cityName,
-      orientation: 'landscape',
-      'per_page': NUMBER_OF_PICTURES.toString(),
-    };
-    const headers = {
-      Authorization: IMAGE_API_KEY,
-    };
-    const recievedImages = await getFetchOf(urlPartsImage, headers);
-    let image = 'images/default-city.jpg';
-    if (typeof recievedImages.photos[0] !== 'undefined') {
-      image = recievedImages.photos[0].src.landscape;
-    }
-    displayCard(recieved, image);
+async function processWeather(cityName) {
+  const NUMBER_OF_DAYS_TO_FORECAST = 8;
+  const NUMBER_OF_PICTURES = 1;
+  const actionWeather = 'forecast';
+  const urlPartsWeather = {
+    site: `http://api.weatherapi.com/v1/${actionWeather}.json`,
+    key: WEATHER_API_KEY,
+    q: cityName,
+    days: NUMBER_OF_DAYS_TO_FORECAST.toString(),
+    aqi: 'no',
+    alerts: 'no',
+  };
+  const recieved = await getFetchOf(urlPartsWeather);
+  const actionImage = 'search';
+  const urlPartsImage = {
+    site: `https://api.pexels.com/v1/${actionImage}`,
+    query: cityName,
+    orientation: 'landscape',
+    'per_page': NUMBER_OF_PICTURES.toString(),
+  };
+  const headers = {
+    Authorization: IMAGE_API_KEY,
+  };
+  const recievedImages = await getFetchOf(urlPartsImage, headers);
+  let image = 'images/default-city.jpg';
+  if (typeof recievedImages.photos[0] !== 'undefined') {
+    image = recievedImages.photos[0].src.landscape;
   }
+  displayCard(recieved, image);
 }
 
 async function getFetchOf(body, headers) {
@@ -84,6 +147,14 @@ async function getFetchOf(body, headers) {
   }
   const jsonData = await response.json();
   return jsonData;
+}
+
+function changeFavorite() {
+  elementById('favorite').classList.toggle('activefavorite');
+  const cityName = elementById('cityname').innerHTML;
+  const index = FAVORITES.indexOf(cityName);
+  if (index > -1) FAVORITES.splice(index, 1);
+  else FAVORITES.push(cityName);
 }
 
 function search(list, searchElement) {
@@ -123,31 +194,27 @@ function displayCard(city, image) {
   insertHTML('card', city.current.last_updated, 'div', 'class="date black-text-shadow"');
 }
 
-// recieved array full of suggestion names
+/**
+ * Display the suggested cities.
+ * @param {array} cities - The array of the suggested names from API.
+ * @param {string} input - The input from the inputbox.
+*/
 function displaySuggestions (cities, input) {
-
-  const foundFavCities = search(FAVORITES, input);
-  cities.forEach((city) => {
-    if (FAVORITES.includes(city[0])) foundFavCities.push(city[0]);
-  });
-  cities = cities.filter((city) => !foundFavCities.includes(city[0]));
-
-  console.log(`Input:${  input}`);
-
+  const recievedCities = processCityLists(cities, input);
+  cities = recievedCities[0];
+  const foundFavCities = recievedCities[1];
   if (elementById('suggestionDropDown')) elementById('suggestionDropDown').remove();
   if ((cities.length > 0 || foundFavCities.length > 0) && document.activeElement.id === 'search'){
-
     insertHTML('inputBox', '', 'div', 'id="suggestionDropDown" class="dropdown is-active"');
     insertHTML('suggestionDropDown', '', 'div', 'id="dropdown-menu" class="dropdown-menu"');
     insertHTML('dropdown-menu', '', 'div', 'id="dropdown-content" class="dropdown-content"');
-
     if (foundFavCities.length > 0) {
       foundFavCities.forEach((cityName, index) => {
         insertHTML('dropdown-content', '', 'button',
           `id="favCity-${index}" class="dropdown-item favorite-dropdown-item"`);
         insertHTML(`favCity-${index}`, '', 'img',
           'class="favorite-dropdown-icon" src="icons/heart-2-fill-black.svg"');
-        elementById(`favCity-${index}`).insertAdjacentHTML('beforeend', `${cityName}`);
+        insertHTML(`favCity-${index}`, cityName, 'span', '');
       });
     }
     if (cities.length > 0 && foundFavCities.length > 0) {
@@ -156,60 +223,22 @@ function displaySuggestions (cities, input) {
     if (cities.length > 0) {
       cities.forEach((cityName, index) => {
         insertHTML('dropdown-content', '', 'button', `id=city-${index} class="dropdown-item"`);
-
-        insertHTML(`city-${index}`, cityName[0], 'span', 'class="dropdown-city"');
         insertHTML(`city-${index}`, cityName[1], 'span', 'class="dropdown-region"');
+        insertHTML(`city-${index}`, cityName[0], 'span', 'class="dropdown-city"');
       });
     }
-
-function myFunction(event) {
-  console.log('asd');
-  elementById('search').value = processWeater(event.target.innerText);
-  console.log(elementById('search').value);
-  processWeater(event.target.firstChild.innerText);
+  }
 }
 
-function makeTheButtonsClickable() {
-  elementById('dropdown-content').addEventListener('click', (event) => myFunction(event));
-}
-
-function processInputChange() {
-  /*elementById('search').addEventListener('change', (event) => {
-    recieveWeather(event);
-    elementById('suggestionDropDown').classList.remove('is-active');
-  });*/
-}
-
-function processFavoriteClick() {
-  elementById('favorite').addEventListener('click', changeFavorite);
-}
-
-function inputAutocomplete() {
-  elementById('search').addEventListener('input', (event) => recieveAutocomplete(event)); // Szöveg változás
-  elementById('search').addEventListener('focusin', (event) => recieveAutocomplete(event)); // Be kattintás változása
-  elementById('search').addEventListener('focusout', (event) => { // Ki kattintás változása
-    setTimeout(function() {
-      if (!document.activeElement.querySelector('button')) {
-        if (event.target.value.length >= 3) {
-          const clickedCity = document.activeElement.firstChild.innerText;
-          event.target.value = clickedCity;
-          recieveWeather(event);
-          elementById('suggestionDropDown').classList.remove('is-active');
-        } else event.target.value = '';
-      } else {
-        elementById('suggestionDropDown').classList.remove('is-active');
-      }
-    }, 0);
+function processCityLists(cities, input) {
+  const foundFavCities = search(FAVORITES, input);
+  cities.forEach((city) => {
+    if (FAVORITES.includes(city[0]) && !foundFavCities.includes(city[0])) {
+      foundFavCities.push(city[0]);
+    }
   });
-
-}
-
-function changeFavorite() {
-  elementById('favorite').classList.toggle('activefavorite');
-  const cityName = elementById('cityname').innerHTML;
-  const index = FAVORITES.indexOf(cityName);
-  if (index > -1) FAVORITES.splice(index, 1);
-  else FAVORITES.push(cityName);
+  cities = cities.filter((city) => !foundFavCities.includes(city[0]));
+  return [cities, foundFavCities];
 }
 
 /**
@@ -223,7 +252,7 @@ function displayInputBar() {
   insertHTML('inputBox', '', 'span', 'id=searchIcon class="icon is-left"');
   insertHTML('searchIcon', '', 'img', 'src=icons/search-line.svg');
   processInputChange();
-  inputAutocomplete();
+  processAutocomplete();
 }
 
 // Create HTML Elements
@@ -239,30 +268,6 @@ function insertHTML(parentElementId, content, tag, attributes) {
   insertElement(elementById(parentElementId),
     createElement(content, tag, attributes));
 }
-
-/*
-/**
- * Add classes.
- * @param {string} id - The id of the HTML element.
- * @param {Array} classes - The array of the classes you want to add to the HTML element.
- */
-/*function addClassesToElement(id, classes) {
-  classes.forEach((className) => {
-    elementById(id).classList.add(className);
-  });
-}
-
-/**
- * Remove classes.
- * @param {string} id - The id of the HTML element.
- * @param {Array} classes - The array of the classes you want to remove from the HTML element.
- */
-/*function removeClassesFromElement(id, classes) {
-  classes.forEach((className) => {
-    elementById(id).classList.remove(className);
-  });
-}
-*/
 
 /**
  * Get an HTML element by its ID.
